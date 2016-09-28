27da47c4768eadd90fe3415b5e4845836e176ddb
Jason Lee <jleeisme@gmail.com>
2016-09-28 16:31:38 -0700

    Included comments in the file
    As it's good to reinforce what is happening

--- dice-roller.js
+++ dice-roller.js
@@ -1,14 +1,16 @@
+// Create a var that gets the users number through process.argv
+// slice(2) as process.argv would include the node and filname paths otherwise - as they're [0] & [1] in the array
 var userNumber = process.argv.slice(2);
+
+// create a function
 function rollDie(){
+  // loop through until you hit the userNumber
   for (var i = 1; i <= userNumber; i++) {
+    // create a var that randomizes a number from 1-6 for the value of userNumber
     var randomNumber = Math.floor(6 * Math.random()) + 1;
+    // print to console
     console.log(randomNumber);
   }
 }
+// call your function to run
 rollDie();
-
-// console.log(userNumber);
-// rollDie();
-
-// var userNumber = process.argv
-// console.log(userNumber);
