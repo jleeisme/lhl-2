52b7d1760b86a34bee0bffe4d2e076ef386401d8
Jason Lee <jleeisme@gmail.com>
2016-09-28 16:34:43 -0700

    Added description of the problem

--- dice-roller.js
+++ dice-roller.js
@@ -1,3 +1,5 @@
+// *** Problem: take a single parameter from the command line, and get a random value from 1-6 for the value of the parameter
+
 // Create a var that gets the users number through process.argv
 // slice(2) as process.argv would include the node and filname paths otherwise - as they're [0] & [1] in the array
 var userNumber = process.argv.slice(2);
